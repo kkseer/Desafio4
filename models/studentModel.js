0f23aebfca69aaaeec048ceba8b15e5ad103043f
carlos.silva <kkseer@gmail.com>
2020-11-11 18:49:11 +0100

    commit inicial

---
+++ models/studentModel.js
@@ -0,0 +1,32 @@
+export default (mongoose) => {
+  const schema = mongoose.Schema({
+    name: {
+      type: String,
+      require: true,
+    },
+
+    subject: {
+      type: String,
+      require: true,
+    },
+
+    type: {
+      type: String,
+      require: true,
+    },
+    value: {
+      type: Number,
+      require: true,
+      //valida se a nota inserida e menor que zero
+      min: 0,
+    },
+    lastModified: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  const studentModel = mongoose.model('grades', schema);
+
+  return studentModel;
+};
