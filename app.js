0711aa591f232854a2a12beb7ca9124025df3f2a
carlos.silva <kkseer@gmail.com>
2020-11-13 10:59:16 +0100

    /

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://desafio4front.herokuapp.com/',
+    origin: 'https://desafio4front.herokuapp.com',
   })
 );
 
